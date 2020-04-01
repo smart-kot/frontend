a9c413b4a4836f99bb27d2ab9fadf13ca3994545
Geylian lingier <geylian.l@gmail.com>
2020-04-01 16:05:03 +0200

    create frontend layout

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <v-app id="inspire">
+    <v-system-bar dark color="#ff6300"></v-system-bar>
+    <Navigators />
+
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <router-view />
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; Smart-kot 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import Navigators from './components/Navigators'
+  export default {
+    components : {
+      Navigators
+    }
+  }
+</script>
+
+<style>
+
+router-link{
+    text-decoration: none;
+}
+
+</style>
---
+++ src/components/Navigators.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+      src="../assets/background.png"
+    >
+      <v-app-bar-nav-icon
+          @click.stop="drawer = !drawer"
+          color="#7e6d6d"
+      />
+        <template v-slot:img="{ props }">
+          <v-img
+            v-bind="props"
+          ></v-img>
+        </template>
+      <v-img
+          alt="smart-kot logo"
+          class="shrink mr-2"
+          contain
+          src="../assets/logo.png"
+          transition="scale-transition"
+          width="200"
+          @click.stop="drawer = !drawer"
+        />
+        <v-spacer></v-spacer>
+        <v-btn icon
+          color="#ffffff"
+          href="https://github.com/smart-kot"
+        >
+          <v-icon>mdi-github</v-icon>
+        </v-btn>
+    </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+      drawer: null,
+    }),
+    created () {
+      this.$vuetify.theme.dark = false;
+    }
+}
+</script>
+
+<style>
+v-app-bar {
+    top:0px;
+}
+</style>
