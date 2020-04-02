e8b5c165bb6325779c62753ad70231af6c90a3b8
Geylian lingier <geylian.l@gmail.com>
2020-04-02 18:15:24 +0200

    add login page

---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'login',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+  h1, p {
+    color: #7e6d6d
+  }
+
+  #cardContent {
+    padding: 20px;
+  }
+</style>
